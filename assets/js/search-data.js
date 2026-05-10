7f409fb6c9dd0fc27dd7f887cbb21cea500f5875
tdnx <tanmaydebnath.ed.uk@gmail.com>
2026-05-10 22:56:55 +0000

    Deploying to gh-pages from @ tdnx/tdnx.github.io@6f27d6f32357b62ec0728000021a16ad2f490a70 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -419,7 +419,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/tanmay_resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
